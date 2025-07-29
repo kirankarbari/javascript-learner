be33dfb2b3f0904d15541db2ca219b8adf921dba
kirankarbari <kirankarbare1819@gmail.com>
2025-07-29 22:06:47 +0530

    Add files via upload

---
+++ convertion.js
@@ -0,0 +1,39 @@
+let score="98as"
+// let score=undefined
+// let score=true
+// let score=false
+
+console.log(typeof score)
+// console.log(typeof(score))
+const valueinnumber=Number(score)
+console.log(valueinnumber);
+
+// "89"-> 89
+// "89ad"->nan (not a number)
+// true->1
+// false->0
+// null->0
+// undefine -> nan
+
+// let isLogin=1
+// let isLogin=0
+// let isLogin="kiran"
+let isLogin=""
+
+let booleanIsLogin=Boolean(isLogin)
+console.log(typeof booleanIsLogin)
+
+console.log(booleanIsLogin)
+
+
+// 1->true
+// 0->false
+// ""->false
+// "vk"->true
+
+let someNumber=343
+
+let stringConvertion=String(someNumber)
+console.log(typeof stringConvertion)
+console.log(stringConvertion)
+ 
---
+++ variable.js
@@ -0,0 +1,17 @@
+const stud_id=3333
+let name="somesh"
+var password="8339920"
+
+// stud_id=90 can't update the const value
+// let values can be updated
+// /var can't be use 
+console.log(stud_id);
+name="ragu"
+password="iiwpkdx"
+city="bidar"
+let state;
+console.table([stud_id,name,password,city,state])
+
+
+
+
